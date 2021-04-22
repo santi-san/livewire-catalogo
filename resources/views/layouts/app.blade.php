5f650ae1c5be0c6efaa3611e26083b4821ecd05b
Santi <it.santiagotoledo@gmail.com>
2021-04-22 16:42:55 -0300

    add fontawesome icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <!-- FAwesome -->
+        <link rel="stylesheet" href="{{asset('vendor/fontawesome/all.min.css')}}">
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
