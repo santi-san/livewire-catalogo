a8df60bece2506cbc0a9bbd102c846bb051c15d5
Santi <it.santiagotoledo@gmail.com>
2021-04-24 00:13:01 -0300

    component and model productos

---
+++ app/Http/Livewire/ShowProductos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ShowProductos extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-productos');
+    }
+}
