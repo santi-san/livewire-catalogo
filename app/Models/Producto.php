512efb7055ca838c90c656dfcf5342850c58aa2b
Santi <it.santiagotoledo@gmail.com>
2021-04-24 21:50:32 -0300

    added relation between productos, marcas & categorias

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -24,4 +24,17 @@ class Producto extends Model
             'prdStock',
             'prdImagen',
         ];
+
+
+        # If your parent model does not use id as its primary key, or you wish to find the associated model using a different column, 
+        # you may pass a third argument to the belongsTo method specifying your parent table's custom key:
+        # return $this->belongsTo(Post::class, 'foreign_key', 'owner_key');
+        public function relMarca() 
+        {
+            return $this->belongsTo( Marca::class, 'idMarca','idMarca' );
+        }
+        public function relCategoria() 
+        {
+            return $this->belongsTo( Categoria::class, 'idCategoria', 'idCategoria' );
+        }
 }
