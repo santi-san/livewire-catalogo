4f8f2c1035bdc80bac612fd4b1728182d211917d
Santi <it.santiagotoledo@gmail.com>
2021-06-21 17:49:57 -0300

    number format for price

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -22,7 +22,7 @@
                 {{-- Sell details --}}
                 <div class="bg-gray-900 rounded-lg p-5 mb-5">
                     <div class="flex justify-between">
-                        <span class="text-xs dark:text-gray-300 text-opacity-50 whitespace-pre-wrap">Nuevo   |   7 vendidos</span>
+                        <span class="text-xs dark:text-gray-300 text-opacity-50 whitespace-pre-wrap">Nuevo   |   {{rand(1,50)}} vendidos</span>
                         <a href="#">
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" viewBox="0 0 22 20"><g stroke-width="1.0" fill-rule="evenodd"><path stroke="#e6bb5d" d="M10.977 2.705C11.93 1.618 13.162 1 14.895 1c3.333 0 5.607 2.152 5.607 6.274 0 .08-.002.16-.005.24-.107 2.596-1.876 5.253-4.737 7.892a33.77 33.77 0 0 1-3.165 2.57 32.447 32.447 0 0 1-1.45.983l-.394.243-.394-.243-.009-.005-.021-.014-.08-.05a32.447 32.447 0 0 1-1.34-.914 33.77 33.77 0 0 1-3.165-2.57c-2.86-2.639-4.63-5.296-4.737-7.892A5.839 5.839 0 0 1 1 7.274C1 3.152 3.274 1 6.607 1c1.733 0 2.966.618 3.918 1.705.056.064.137.165.226.282.09-.117.17-.218.226-.282z"></path></g></svg>
                     </a>
@@ -37,7 +37,7 @@
                          $price = $product->price / 3;
                      @endphp
                         <p class="text-sm">
-                            en 3 cuotas de <span class="text-yellow-300">$ {{round($price, 2)}}</span> sin interés.
+                            en 3 cuotas de <span class="text-yellow-300">$ {{ number_format($price, 2)}}</span> sin interés.
                         </p>
                         <a href="#" class="text-sm text-yellow-300">Ver los medios de pago</a>
                     </div>
